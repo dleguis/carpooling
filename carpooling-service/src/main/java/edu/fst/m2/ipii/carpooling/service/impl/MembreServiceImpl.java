c449ce9f258686b659bfe71405dd29b201ad1a42
gobedson <gobedson1@gmail.com>
2015-04-08 21:26:20 +0200

    webservices new user and authentification

--- carpooling-service/src/main/java/edu/fst/m2/ipii/carpooling/service/impl/MembreServiceImpl.java
+++ carpooling-service/src/main/java/edu/fst/m2/ipii/carpooling/service/impl/MembreServiceImpl.java
@@ -14,36 +14,46 @@ import java.util.List;
  * Created by Dimitri on 02/04/15.
  */
 @Service
-public class MembreServiceImpl extends AbstractServiceImpl implements MembreService {
+public class MembreServiceImpl extends AbstractServiceImpl implements
+		MembreService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MembreServiceImpl.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(MembreServiceImpl.class);
 
-    @Override
-    public List<MembreDto> rechercher() {
-        List<Membre> membres = membreRepository.findAll();
-        for(Membre membre : membres) {
-            LOGGER.debug("profils : {}", membre.getProfils());
-        }
-        return MapperUtils.map(mapperService, membres, MembreDto.class);
-    }
+	@Override
+	public List<MembreDto> rechercher() {
+		List<Membre> membres = membreRepository.findAll();
+		for (Membre membre : membres) {
+			LOGGER.debug("profils : {}", membre.getProfils());
+		}
+		return MapperUtils.map(mapperService, membres, MembreDto.class);
+	}
 
-    @Override
-    public MembreDto rechercher(int id) {
-        Membre membre = membreRepository.findOne(id);
+	@Override
+	public MembreDto rechercher(int id) {
+		Membre membre = membreRepository.findOne(id);
 
-        LOGGER.debug("profils : {}", membre.getProfils());
-        LOGGER.debug("voitures : {}", membre.getVoitures());
+		LOGGER.debug("profils : {}", membre.getProfils());
+		LOGGER.debug("voitures : {}", membre.getVoitures());
 
-        return MapperUtils.map(mapperService, membre, MembreDto.class);
-    }
+		return MapperUtils.map(mapperService, membre, MembreDto.class);
+	}
 
-    @Override
-    public MembreDto rechercher(String login) {
-        Membre membre = membreRepository.findByLogin(login);
+	@Override
+	public MembreDto rechercher(String login) {
+		Membre membre = membreRepository.findByLogin(login);
 
-        LOGGER.debug("profils : {}", membre.getProfils());
-        LOGGER.debug("voitures : {}", membre.getVoitures());
+		LOGGER.debug("profils : {}", membre.getProfils());
+		LOGGER.debug("voitures : {}", membre.getVoitures());
 
-        return MapperUtils.map(mapperService, membre, MembreDto.class);
-    }
+		return MapperUtils.map(mapperService, membre, MembreDto.class);
+	}
+
+	@Override
+	public void save(MembreDto membre) {
+		Membre m = new Membre();
+		m = MapperUtils.map(mapperService, membre, Membre.class);
+		membreRepository.save(m);
+
+	}
 }
