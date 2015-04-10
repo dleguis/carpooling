41387e71ce0fbc037fef2a09628a9580d8d1952a
Kawada <kawada59@gmail.com>
2015-04-10 10:01:35 +0200

    Commit pré-soutenance

---
+++ carpooling-presentation/src/main/java/edu/fst/m2/ipii/carpooling/presentation/model/UserLoginModel.java
@@ -0,0 +1,28 @@
+package edu.fst.m2.ipii.carpooling.presentation.model;
+
+/**
+ * Created by Dimitri on 10/04/2015.
+ */
+public class UserLoginModel {
+
+    private String login;
+
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- carpooling-presentation/src/main/java/edu/fst/m2/ipii/carpooling/presentation/rest/MembreRestController.java
+++ carpooling-presentation/src/main/java/edu/fst/m2/ipii/carpooling/presentation/rest/MembreRestController.java
@@ -1,16 +1,12 @@
 package edu.fst.m2.ipii.carpooling.presentation.rest;
 
-import edu.fst.m2.ipii.carpooling.presentation.AbstractController;
-import edu.fst.m2.ipii.carpooling.presentation.model.UserLoginForm;
+import edu.fst.m2.ipii.carpooling.presentation.model.UserLoginModel;
 import edu.fst.m2.ipii.carpooling.service.MembreService;
 import edu.fst.m2.ipii.carpooling.transverse.dto.MembreDto;
-import edu.fst.m2.ipii.carpooling.transverse.dto.TrajetDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by Dimitri on 09/04/2015.
@@ -23,7 +19,7 @@ public class MembreRestController {
     private MembreService membreService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public MembreDto getMembre(UserLoginForm userLoginForm) {
-        return membreService.rechercher(userLoginForm.getLogin(), userLoginForm.getPassword());
+    public MembreDto getMembre(UserLoginModel userLoginModel) {
+        return membreService.rechercher(userLoginModel.getLogin(), userLoginModel.getPassword());
     }
 }
